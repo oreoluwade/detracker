497af05e4efbaf24cb35bc8a83b8ce16c572856c
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-03-27 15:28:37 +0100

    add additional contorllers and endpoints

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,7 +4,7 @@ import models from '../models';
 const { User } = models;
 
 export default {
-  async createUser (req, res) {
+  async createUser(req, res) {
     try {
       const user = await User.create(req.body);
       return res.status(201).json({ user, message: 'User creation Successful' });
@@ -16,7 +16,7 @@ export default {
     }
   },
 
-  async getUser (req, res) {
+  async getUser(req, res) {
     try {
       const id = parseInt(req.params.id);
       if (isNaN(id)) {
@@ -27,6 +27,60 @@ export default {
         return res.status(404).json({ message: 'No user with the given ID' });
       }
       return res.status(200).json({ user });
+    } catch (err) {
+      return res.status(500).json({ error: 'An Unknown error occured' });
+    }
+  },
+
+  async editUserDetails(req, res) {
+    try {
+      const id = parseInt(req.params.id);
+      if (isNaN(id)) {
+        return res.status(400).json({ error : 'ID should be a number' })
+      }
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: 'No user with the given ID' });
+      }
+      const updatedUser = await user.update(req.body);
+
+      return res.status(200).json({ updatedUser, message: 'Details updated!' });
+    } catch (err) {
+      if (err instanceof Sequelize.ValidationError) {
+        return res.status(409).json({ error: 'Username/email already in use' });
+      }
+      return res.status(500).json({ error: 'An Unknown error occured' });
+    }
+  },
+
+  async fetchAllUsers(req, res) {
+    try {
+      const users = await User.findAll();
+      if (users[0]) {
+        return res.json({ users })
+      }
+      return res.status(404).json({
+        message: 'No users created yet',
+        createNewUser: `<a href='http:localhost:8081/register'>Add new User</a>`
+      });
+    } catch (err) {
+      return res.status(500).json({error: 'An unknown error occured'});
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const id = parseInt(req.params.id)
+      if (isNaN(id)) {
+        return res.status(400).json({ error : 'ID should be a number' })
+      }
+      const user = await User.destroy({
+        where: { id },
+      });
+      if (user === 1) {
+        return res.status(200).json({ message: 'User Removed' });
+      }
+      return res.status(404).json({ message: 'No user with the given ID' });
     } catch (err) {
       return res.status(500).json({ error: 'An Unknown error occured' });
     }
--- server/routes.js
+++ server/routes.js
@@ -5,8 +5,13 @@ const router = express.Router();
 
 router.route('/user/:id')
   .get(UsersController.getUser)
+  .put(UsersController.editUserDetails)
+  .delete(UsersController.deleteUser)
 
 router.route('/register')
   .post(UsersController.createUser)
+
+router.route('/users')
+  .get(UsersController.fetchAllUsers);
 
 export default router;
